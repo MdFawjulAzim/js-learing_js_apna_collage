9c2420a7fc8fb3ad9a2915605878765d8573c37c
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-09-06 01:48:58 +0600

    filter function

--- first.js
+++ first.js
@@ -359,10 +359,10 @@
 
 // console.log(newValue);
 
-let arr =[22,32,4   ];
+let arr =[1,2,3,4,5,6,7,8,9,10   ];
 
-let newValue =arr.map((val)=>{
-   return val **2;
+let newValue =arr.filter((val)=>{
+   return val % 2 === 0;
 })
 
 console.log(newValue);

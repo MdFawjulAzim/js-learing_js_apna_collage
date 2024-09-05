a6870940959d7f07499a63e8fe61c44501c065f5
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-09-06 01:32:17 +0600

    foreach prac

--- first.js
+++ first.js
@@ -342,3 +342,11 @@
 // arr.forEach((val)=>{
 //     console.log(val.toUpperCase());
 // })
+
+const numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach(function(number) {
+  console.log(number);
+});
+numbers.forEach((number) => {console.log(number)});
+

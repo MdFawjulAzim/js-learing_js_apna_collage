e0e85ceda5b929e525be322c66b97d32673fc92f
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-10-05 03:00:22 +0600

    promise er maddome api call

--- API/app.js
+++ API/app.js
@@ -2,24 +2,35 @@ const URL = "https://cat-fact.herokuapp.com/facts";
 const factPara = document.getElementById("fact");
 
 const btn = document.getElementById("btn");
-btn.innerText="";
+// btn.innerText="";
 
 
 
 
-const getFacts = async ()=>{
-    console.log("getting DAta...");
-    let response = await fetch(URL);
-    console.log(response);
-    console.log("json format");
-    let data = await response .json();
-    console.log(data[1].text);
+// const getFacts = async ()=>{
+//     console.log("getting DAta...");
+//     let response = await fetch(URL);
+//     console.log(response);
+//     console.log("json format");
+//     let data = await response .json();
+//     console.log(data[1].text);
 
-    factPara.innerText = data[0].text;
+//     factPara.innerText = data[0].text;
 
-    console.log("Data fetched");
-    btn.innerText="Get facts";
+//     console.log("Data fetched");
+//     btn.innerText="Get facts";
  
-}
-btn.addEventListener("click", getFacts);
+// }
+// btn.addEventListener("click", getFacts);
 
+function getFacts() {
+    fetch(URL).then((response)=>{
+        return response.json();
+    }).then((data)=>{
+        console.log(data);
+        factPara.innerText = data[0].text;
+    })
+}
+
+btn.addEventListener("click", getFacts);
+getFacts();

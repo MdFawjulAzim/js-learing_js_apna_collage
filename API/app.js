033c3a0fddbbc4184bcaf5d11576aac89b83d369
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-10-05 02:55:20 +0600

    API Test

---
+++ API/app.js
@@ -0,0 +1,25 @@
+const URL = "https://cat-fact.herokuapp.com/facts";
+const factPara = document.getElementById("fact");
+
+const btn = document.getElementById("btn");
+btn.innerText="";
+
+
+
+
+const getFacts = async ()=>{
+    console.log("getting DAta...");
+    let response = await fetch(URL);
+    console.log(response);
+    console.log("json format");
+    let data = await response .json();
+    console.log(data[1].text);
+
+    factPara.innerText = data[0].text;
+
+    console.log("Data fetched");
+    btn.innerText="Get facts";
+ 
+}
+btn.addEventListener("click", getFacts);
+
